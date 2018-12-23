1932f4fce2f2d53344bb53f4ed9732dfae3125f6
KeybrL <keyboard-l@outlook.com>
2018-12-23 21:43:46 +0800

    微小的贡献

---
+++ src/lexer/dfa.py
@@ -0,0 +1,19 @@
+from enum import unique, Enum
+
+
+@unique
+class Token(Enum):
+    NUM = 1  # 常数
+    T = 2  # 参数 T
+    FUNC = 3  # 函数
+    KEY_WORD = 4  # 关键字
+    
+
+
+
+
+DFA = (
+    {
+        "type": ""
+    }
+)
